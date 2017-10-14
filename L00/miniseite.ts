d9d8daf59c89a8041c1fbb394008d0c820293774
Stephen Bodmer <stephen.bodmer@hs-furtwangen.de>
2017-10-14 13:09:05 +0200

    Add files via upload
    .

---
+++ L00/miniseite.ts
@@ -0,0 +1,7 @@
+//window.addEventListener("load", init);
+function huhu() {
+    var name: string = prompt("Wie heißt du?");
+    if (name != null) {
+        alert("Grüß Gott, " + name + ".");
+    }
+}
