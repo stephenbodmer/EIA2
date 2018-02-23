65f70abad00fd4720b5e848fc94929e7e5c1b5f3
Stephen Bodmer <stephen.bodmer@hs-furtwangen.de>
2018-02-23 20:36:27 +0100

    .

--- abschlussaufgabe/main.js
+++ abschlussaufgabe/main.js
@@ -5,8 +5,8 @@ var abschluss;
     var score = 0;
     var end = false;
     var fallingMeteor = [];
-    // var movingRakete: Rakete[]=[];
-    var movingRakete;
+    var movingRakete = [];
+    // let movingRakete: Rakete = new Rakete(2,3);
     function spielwiese() {
         var canvas = document.getElementsByTagName("canvas")[0];
         abschluss.r = canvas.getContext("2d");
@@ -44,101 +44,45 @@ var abschluss;
         }
         window.setTimeout(animate, 10);
     }
-    function buttondraw() {
-        var buttonup = document.createElement("button");
-        buttonup.innerText = "UP";
-        buttonup.id = "ButtonUp";
-        buttonup.addEventListener("click", moveUp);
-        document.body.appendChild(buttonup);
-        var buttonleft = document.createElement("button");
-        buttonleft.innerText = "LEFT";
-        buttonleft.id = "ButtonLeft";
-        buttonleft.addEventListener("click", moveLeft);
-        document.body.appendChild(buttonleft);
-        var buttonright = document.createElement("button");
-        buttonright.innerText = "RIGHT";
-        buttonright.id = "ButtonRight";
-        buttonright.addEventListener("click", moveRight);
-        document.body.appendChild(buttonright);
-        var buttondown = document.createElement("button");
-        buttondown.innerText = "DOWN";
-        buttondown.id = "ButtonDown";
-        buttondown.addEventListener("click", moveDown);
-        document.body.appendChild(buttondown);
-    }
-    function moveUp() {
-        movingRakete.y -= 10;
-    }
-    function moveDown() {
-        movingRakete.y += 10;
-    }
-    function moveLeft() {
-        movingRakete.x -= 10;
-    }
-    function moveRight() {
-        movingRakete.x += 10;
-    }
+    //
+    // function buttondraw(): void {
+    //     let buttonup: HTMLButtonElement = document.createElement("button");
+    //     buttonup.innerText = "UP";
+    //     buttonup.id = "ButtonUp";
+    //     buttonup.addEventListener("click", moveUp);
+    //     document.body.appendChild(buttonup);
+    //
+    //     let buttonleft: HTMLButtonElement = document.createElement("button");
+    //     buttonleft.innerText = "LEFT";
+    //     buttonleft.id = "ButtonLeft";
+    //     buttonleft.addEventListener("click", moveLeft);
+    //     document.body.appendChild(buttonleft);
+    //
+    //     let buttonright: HTMLButtonElement = document.createElement("button");
+    //     buttonright.innerText = "RIGHT";
+    //     buttonright.id = "ButtonRight";
+    //     buttonright.addEventListener("click", moveRight);
+    //     document.body.appendChild(buttonright);
+    //
+    //     let buttondown: HTMLButtonElement = document.createElement("button");
+    //     buttondown.innerText = "DOWN";
+    //     buttondown.id = "ButtonDown";
+    //     buttondown.addEventListener("click", moveDown);
+    //     document.body.appendChild(buttondown);
+    // }
+    //
+    //
+    // function moveUp(): void {
+    //     movingRakete.y -= 10;
+    // }
+    // function moveDown(): void {
+    //     movingRakete.y += 10;
+    // }
+    // function moveLeft(): void {
+    //     movingRakete.x -= 10;
+    // }
+    // function moveRight(): void {
+    //     movingRakete.x += 10;
+    // }
 })(abschluss || (abschluss = {}));
-// namespace abschluss{
-//
-// export let r: CanvasRenderingContext2D;
-//
-// window.addEventListener("load", spielwiese);
-//
-// let imgData: ImageData;
-// let fallingMeteor: Meteorschauer[] = [];
-// let movingRakete: Rakete[]=[];
-// //let movingWolken: Wolken[]=[];
-//
-// function spielwiese(): void {
-//
-//   let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
-//   r = canvas.getContext("2d");
-//
-//     //Background
-//     r.fillStyle = "#071019";
-//     r.fillRect(0, 0, 800, 600);
-//
-//     //Sun
-//     r.beginPath();
-//     r.strokeStyle = "#B43104";
-//     r.arc(700, 100, 50, 0, 2 * Math.PI);
-//     r.stroke();
-//     r.fillStyle = "#B43104";
-//     r.fill();
-//
-//
-//     // Meteorschauer
-//     for (let i: number = 0; i < 10; i++) {
-//         fallingMeteor[i] = new Meteorschauer(Math.random() * 800, Math.random() * 600);
-//     }
-//     for (let i: number = 0; i < 10; i++) {
-//         movingRakete[i] = new Rakete(Math.random() * 800, Math.random() * 600);
-//     }
-//
-//
-//     imgData = r.getImageData(0, 0, canvas.width, canvas.height);
-//
-//     animate();
-// }
-// // Animate
-// function animate(): void {
-//
-//     r.putImageData(imgData, 0, 0);    //Hintergrundbild einsetzen
-//
-//     //Schnee
-//     for (let i: number = 0; i < fallingMeteor.length; i++) {
-//         let meteor: Meteorschauer = fallingMeteor[i];
-//         meteor.moveMeteor();
-//     }
-//
-//     for (let i: number = 0; i < movingRakete.length; i++) {
-//         let ra: Rakete = movingRakete[i];
-//         ra.moveRakete();
-//     }
-//
-//
-//
-//     window.setTimeout(animate, 10);
-// }
 //# sourceMappingURL=main.js.map
--- abschlussaufgabe/main.ts
+++ abschlussaufgabe/main.ts
@@ -9,8 +9,8 @@ let score: number = 0;
 let end: boolean = false;
 
 var fallingMeteor: Meteorschauer[] = [];
-// var movingRakete: Rakete[]=[];
-let movingRakete: Rakete;
+var movingRakete: Rakete[]=[];
+// let movingRakete: Rakete = new Rakete(2,3);
 
 
 function spielwiese(): void {
@@ -44,10 +44,6 @@ function spielwiese(): void {
     }
 
 
-
-
-
-
 // Animate
 function animate(): void {
 
@@ -66,112 +62,45 @@ function animate(): void {
 
     window.setTimeout(animate, 10);
 }
-
-function buttondraw(): void {
-    let buttonup: HTMLButtonElement = document.createElement("button");
-    buttonup.innerText = "UP";
-    buttonup.id = "ButtonUp";
-    buttonup.addEventListener("click", moveUp);
-    document.body.appendChild(buttonup);
-
-    let buttonleft: HTMLButtonElement = document.createElement("button");
-    buttonleft.innerText = "LEFT";
-    buttonleft.id = "ButtonLeft";
-    buttonleft.addEventListener("click", moveLeft);
-    document.body.appendChild(buttonleft);
-
-    let buttonright: HTMLButtonElement = document.createElement("button");
-    buttonright.innerText = "RIGHT";
-    buttonright.id = "ButtonRight";
-    buttonright.addEventListener("click", moveRight);
-    document.body.appendChild(buttonright);
-
-    let buttondown: HTMLButtonElement = document.createElement("button");
-    buttondown.innerText = "DOWN";
-    buttondown.id = "ButtonDown";
-    buttondown.addEventListener("click", moveDown);
-    document.body.appendChild(buttondown);
-}
-
-
-function moveUp(): void {
-    movingRakete.y -= 10;
-}
-function moveDown(): void {
-    movingRakete.y += 10;
-}
-function moveLeft(): void {
-    movingRakete.x -= 10;
-}
-function moveRight(): void {
-    movingRakete.x += 10;
-}
-
-}
-
-
-// namespace abschluss{
 //
-// export let r: CanvasRenderingContext2D;
+// function buttondraw(): void {
+//     let buttonup: HTMLButtonElement = document.createElement("button");
+//     buttonup.innerText = "UP";
+//     buttonup.id = "ButtonUp";
+//     buttonup.addEventListener("click", moveUp);
+//     document.body.appendChild(buttonup);
 //
-// window.addEventListener("load", spielwiese);
+//     let buttonleft: HTMLButtonElement = document.createElement("button");
+//     buttonleft.innerText = "LEFT";
+//     buttonleft.id = "ButtonLeft";
+//     buttonleft.addEventListener("click", moveLeft);
+//     document.body.appendChild(buttonleft);
 //
-// let imgData: ImageData;
-// let fallingMeteor: Meteorschauer[] = [];
-// let movingRakete: Rakete[]=[];
-// //let movingWolken: Wolken[]=[];
+//     let buttonright: HTMLButtonElement = document.createElement("button");
+//     buttonright.innerText = "RIGHT";
+//     buttonright.id = "ButtonRight";
+//     buttonright.addEventListener("click", moveRight);
+//     document.body.appendChild(buttonright);
 //
-// function spielwiese(): void {
-//
-//   let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
-//   r = canvas.getContext("2d");
-//
-//     //Background
-//     r.fillStyle = "#071019";
-//     r.fillRect(0, 0, 800, 600);
-//
-//     //Sun
-//     r.beginPath();
-//     r.strokeStyle = "#B43104";
-//     r.arc(700, 100, 50, 0, 2 * Math.PI);
-//     r.stroke();
-//     r.fillStyle = "#B43104";
-//     r.fill();
+//     let buttondown: HTMLButtonElement = document.createElement("button");
+//     buttondown.innerText = "DOWN";
+//     buttondown.id = "ButtonDown";
+//     buttondown.addEventListener("click", moveDown);
+//     document.body.appendChild(buttondown);
+// }
 //
 //
-//     // Meteorschauer
-//     for (let i: number = 0; i < 10; i++) {
-//         fallingMeteor[i] = new Meteorschauer(Math.random() * 800, Math.random() * 600);
-//     }
-//     for (let i: number = 0; i < 10; i++) {
-//         movingRakete[i] = new Rakete(Math.random() * 800, Math.random() * 600);
-//     }
-//
-//
-//     imgData = r.getImageData(0, 0, canvas.width, canvas.height);
-//
-//     animate();
+// function moveUp(): void {
+//     movingRakete.y -= 10;
+// }
+// function moveDown(): void {
+//     movingRakete.y += 10;
+// }
+// function moveLeft(): void {
+//     movingRakete.x -= 10;
+// }
+// function moveRight(): void {
+//     movingRakete.x += 10;
 // }
 
-
-
-// // Animate
-// function animate(): void {
-//
-//     r.putImageData(imgData, 0, 0);    //Hintergrundbild einsetzen
-//
-//     //Schnee
-//     for (let i: number = 0; i < fallingMeteor.length; i++) {
-//         let meteor: Meteorschauer = fallingMeteor[i];
-//         meteor.moveMeteor();
-//     }
-//
-//     for (let i: number = 0; i < movingRakete.length; i++) {
-//         let ra: Rakete = movingRakete[i];
-//         ra.moveRakete();
-//     }
-//
-//
-//
-//     window.setTimeout(animate, 10);
-// }
+}
